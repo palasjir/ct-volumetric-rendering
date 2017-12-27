56a966c647ce0657b0bda5cddbcca4ddbcaed45e
Jiří Palas <palas.jiri@gmail.com>
2017-12-27 19:22:17 +0100

    split interpolation to different block

--- src/main/kotlin/palasjir/viewer/utils/VolumeData.kt
+++ src/main/kotlin/palasjir/viewer/utils/VolumeData.kt
@@ -16,24 +16,18 @@ class VolumeData {
 
     init {
         loadImages()
+        interpolate()
     }
 
     private fun loadImages() {
-
-        var tempImage: BufferedImage
-        var tempRaster: WritableRaster
-
-        println("Loading images ...")
-
-        for (z in 0..IMG_DEPTH_ORIG - 1) {
-            val file = File(IMAGES_DIR + String.format("%03d", z + 1) + ".png")
+        for (z in 0 until IMG_DEPTH_ORIG) {
+            val file = imageFile(z)
             try {
-                tempImage = ImageIO.read(file)
-                tempRaster = tempImage.raster
-                for (x in 0..tempImage.width - 1) {
-                    for (y in 0..tempImage.height - 1) {
-                        val value = tempRaster.getSample(x, y, 0)
-                        pixels[x][y][z * 2] = value
+                val tempImage = ImageIO.read(file)
+                val tempRaster = tempImage.raster
+                for (x in 0 until tempImage.width) {
+                    for (y in 0 until tempImage.height) {
+                        pixels[x][y][z * 2] = tempRaster.getSample(x, y, 0)
                     }
                 }
             } catch (ex: IOException) {
@@ -42,29 +36,27 @@ class VolumeData {
             }
 
         }
-        println("Loading done.")
+    }
 
-        println("Start interpolating done ...")
+    private fun interpolate() {
         var i = 1
         while (i < IMG_DEPTH - 1) {
-            for (x in 0..IMG_WIDTH - 1) {
-                for (y in 0..IMG_HEIGHT - 1) {
+            for (x in 0 until IMG_WIDTH) {
+                for (y in 0 until IMG_HEIGHT) {
                     pixels[x][y][i] = (pixels[x][y][i - 1] + pixels[x][y][i + 1]) / 2
                 }
             }
             i += 2
         }
-        println("Interpolating done.")
-
     }
 
     val buffer: ByteBuffer
         get() {
             val size = IMG_WIDTH * IMG_HEIGHT * IMG_DEPTH * PIXELS_IN_BUFFER
             val buffer = ByteBuffer.allocate(size)
-            for (z in 1..IMG_DEPTH - 1) {
-                for (y in 0..IMG_HEIGHT - 1) {
-                    for (x in 0..IMG_WIDTH - 1) {
+            for (z in 1 until IMG_DEPTH) {
+                for (y in 0 until IMG_HEIGHT) {
+                    for (x in 0 until IMG_WIDTH) {
                         buffer.put(scaleToByte(pixels[x][y][z]))
                     }
                 }
@@ -80,9 +72,9 @@ class VolumeData {
     fun gradientsBuffer(): FloatBuffer {
         val size = IMG_VOXELS * CHANNELS
         val buffer = FloatBuffer.allocate(size)
-        for (z in 0..IMG_DEPTH - 1) {
-            for (y in 0..IMG_HEIGHT - 1) {
-                for (x in 0..IMG_WIDTH - 1) {
+        for (z in 0 until IMG_DEPTH) {
+            for (y in 0 until IMG_HEIGHT) {
+                for (x in 0 until IMG_WIDTH) {
                     buffer.put((if (x == 0 || x == IMG_WIDTH - 1) 0 else pixels[x - 1][y][z] - pixels[x + 1][y][z]).toFloat())
                     buffer.put((if (y == 0 || y == IMG_HEIGHT - 1) 0 else pixels[x][y - 1][z] - pixels[x][y + 1][z]).toFloat())
                     buffer.put((if (z == 0 || z == IMG_DEPTH - 1) 0 else pixels[x][y][z - 1] - pixels[x][y][z + 1]).toFloat())
@@ -91,6 +83,10 @@ class VolumeData {
         }
         buffer.rewind()
         return buffer
+    }
+
+    private fun imageFile(i: Int): File {
+        return File(IMAGES_DIR + String.format("%03d", i + 1) + ".png")
     }
 
     companion object {
