72114dc40641622588a4ea117ae682f2eb6ac13f
Jiří Palas <palas.jiri@gmail.com>
2017-12-27 19:23:02 +0100

    removed unused imports

--- src/main/kotlin/palasjir/viewer/utils/VolumeData.kt
+++ src/main/kotlin/palasjir/viewer/utils/VolumeData.kt
@@ -1,7 +1,5 @@
 package palasjir.viewer.utils
 
-import java.awt.image.BufferedImage
-import java.awt.image.WritableRaster
 import java.io.File
 import java.io.IOException
 import java.nio.ByteBuffer
